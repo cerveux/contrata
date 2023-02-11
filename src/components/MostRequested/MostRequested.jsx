fa94594d1cb00af1cbb95eeb4f5d9e641d3a6427
cerveux <diegomartinguzman@hotmail.com.ar>
2023-02-11 10:27:12 -0300

    arreglado el carrousel en chrome

--- src/components/MostRequested/MostRequested.jsx
+++ src/components/MostRequested/MostRequested.jsx
@@ -2,51 +2,44 @@ import { useRef, useEffect, useState } from "react";
 import Card from "./Card";
 import data from "./data.json";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faChevronCircleRight, faCircleChevronLeft} from '@fortawesome/free-solid-svg-icons' 
+import { faChevronCircleRight, faCircleChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
 const MostRequested = () => {
 
   const carousel = useRef(null);
-  const maxIndex = data.resources.length;
   const [currentIndex, setCurrentIndex] = useState(0);
 
 
 
   const scrollLeft = () => {
-    console.log(carousel.current)
-    if(carousel.current.scrollLeft === 0){
-      console.log(carousel.current.scrollLeftMax)
-      let newIndex = Math.floor(carousel.current.scrollLeftMax / 301);
-      console.log(newIndex)
-      setCurrentIndex(newIndex);
-    }else{
-      setCurrentIndex(currentIndex-1);
-    }
-    
-  
-    
-    
+    let maxWidth = carousel.current.scrollWidth-carousel.current.clientWidth;
+    if (carousel.current.scrollLeft === 0) {
 
+      let newIndex = Math.floor(maxWidth / 301);
+      setCurrentIndex(newIndex);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
   }
 
   const scrollrigth = () => {
-    console.log(carousel.current)
-    if(carousel.current.scrollLeft === carousel.current.scrollLeftMax || ((carousel.current.scrollLeftMax - carousel.current.scrollLeft) < 100)){
+    let maxWidth = carousel.current.scrollWidth-carousel.current.clientWidth;
+    if (carousel.current.scrollLeft === maxWidth || ((maxWidth - carousel.current.scrollLeft) < 100)) {
       setCurrentIndex(0);
-    }else{
-      setCurrentIndex(currentIndex+1)
+    } else {
+      setCurrentIndex(currentIndex + 1)
     }
   }
 
+
   useEffect(() => {
-    console.log(currentIndex);
     carousel.current["scrollLeft"] =
-        301 * currentIndex;
+      301 * currentIndex;
   }, [currentIndex]);
 
   return (
     <div className="2xl:container 2xl:mx-auto 2xl:px-0 py-3 md:px-10 h-500 relative">
-      
+
       <section className="carousel my-20 mx-auto font-poppins min-w  max-w-[1308px] h-[415px] ">
         <div className="ml-16 mb-4 text-left">
           <h3 className="text-[#28315C] font-extrabold text-3l ">
@@ -56,7 +49,7 @@ const MostRequested = () => {
         <div className="prueba flex">
           <button onClick={scrollLeft} className=" rounded-full  text-3xl text-[#28315C] font-bold my-auto m-2 sm:ml-2 sm:mr-[14px] flex items-center hover:scale-110 "> <FontAwesomeIcon icon={faCircleChevronLeft} /> </button>
 
-          <div id="carousel" className="carousel my-4 py-3 p-0 p-1 sm:px-3 flex flex-grow items-center justify-start overflow-x-hidden scroll-smooth snap-mandatory touch-pan-x z-0 gap-6 "
+          <div id="carousel" className="carousel my-4 py-3 p-0 sm:px-3 flex flex-grow items-center justify-start overflow-x-hidden scroll-smooth snap-mandatory touch-pan-x z-0 gap-6 "
             ref={carousel}>
 
             {data.resources.map((requested, index) => {
